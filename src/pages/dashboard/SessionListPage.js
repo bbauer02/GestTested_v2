7f1110beb34f5ec374150487b9b75582fc19e658
bbauer02 <bbauer02@gmail.com>
2023-02-02 21:25:21 +0100

    sauvegarde japon

--- src/pages/dashboard/SessionListPage.js
+++ src/pages/dashboard/SessionListPage.js
@@ -1,15 +1,22 @@
 import { Helmet } from 'react-helmet-async';
+import { useState, useEffect } from 'react';
+import { useNavigate, Link as RouterLink } from 'react-router-dom';
 // @mui
 import {
+    Button,
+    Card,
+    Table,
+    Tooltip,
+    TableBody,
     Container,
-    Card
+    IconButton,
+    TableContainer,
 } from '@mui/material';
 // redux
 import { useDispatch, useSelector } from '../../redux/store';
-import {deleteExam, getExams} from '../../redux/slices/exam';
+import { getSessionsFiltered } from '../../redux/slices/session';
 // routes
 import { PATH_DASHBOARD } from '../../routes/paths';
-import CustomBreadcrumbs from '../../components/custom-breadcrumbs';
 // components
 import { useSettingsContext } from '../../components/settings';
 import {
@@ -25,8 +32,10 @@ import {
 } from '../../components/table';
 import Iconify from '../../components/iconify';
 import Scrollbar from '../../components/scrollbar';
+import CustomBreadcrumbs from '../../components/custom-breadcrumbs';
 import ConfirmDialog from '../../components/confirm-dialog';
-
+// sections
+import { SessionTableRow, SessionTableToolbar} from "../../sections/@dashboard/session/list";
 // ----------------------------------------------------------------------
 const TABLE_HEAD = [
     { id: 'session_id', label: 'ID', align: 'left'},
@@ -36,28 +45,262 @@ const TABLE_HEAD = [
     { id: 'subscribers', label: 'Inscrits', align: 'left' },
     { id: 'validation', label: 'Validation', align: 'left' },
     { id: 'action', label: 'Action', align: 'left' },
+    { id: '' },
 ];
-
+// ----------------------------------------------------------------------
 
 export default function SessionListPage() {
+    const {
+        dense,
+        page,
+        order,
+        orderBy,
+        rowsPerPage,
+        setPage,
+        //
+        selected,
+        setSelected,
+        onSelectRow,
+        onSelectAllRows,
+        //
+        onSort,
+        onChangeDense,
+        onChangePage,
+        onChangeRowsPerPage,
+    } = useTable({
+        defaultOrderBy: 'label',
+    });
     const { themeStretch } = useSettingsContext();
+
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+
+    const { session, isLoading} = useSelector((state) => state.session);
+
+    const [tableData, setTableData] = useState([]);
+
+    const [filterName, setFilterName] = useState('');
+
+    const [openConfirm, setOpenConfirm] = useState(false);
+
+    useEffect(() => {
+        dispatch(getInstituts());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (instituts.length) {
+            setTableData(instituts);
+        }
+    }, [instituts]);
+
+    const dataFiltered = applyFilter({
+        inputData : tableData,
+        comparator: getComparator(order, orderBy),
+        filterName,
+    });
+
+    const denseHeight = dense ? 60 : 80;
+
+    const isNotFound = (!dataFiltered.length && !!filterName) || (!isLoading && !dataFiltered.length);
+
+    const handleOpenConfirm = () => {
+        setOpenConfirm(true);
+    };
+
+    const handleCloseConfirm = () => {
+        setOpenConfirm(false);
+    };
+    const handleDeleteRow = (id) => {
+        // dispatch(deleteInstitut(id));
+    };
+
+    const handleDeleteRows = (selectedRow) => {
+        // const deleteRows = tableData.filter((row) => !selectedRow.includes(row.institut_id));
+        // setSelected([]);
+        // setTableData(deleteRows);
+    };
+
+    const handleEditRow = (institutId) => {
+        navigate(PATH_DASHBOARD.admin.institut.edit(institutId));
+    };
+
+    const handleDetailRow = (institutId) => {
+        navigate(PATH_DASHBOARD.admin.institut.details(institutId));
+    };
+
+    const handleUsersList = (institutId) => {
+        navigate(PATH_DASHBOARD.admin.institut.users(institutId));
+    };
+
+    const handleExaminatorsList = (institutId) => {
+        navigate(PATH_DASHBOARD.admin.institut.examinators(institutId));
+    };
+
+    const handleSessionsList = (institutId) => {
+        navigate(PATH_DASHBOARD.admin.institut.sessions(institutId));
+    };
+
+    const handlePricesList = (institutId) => {
+        navigate(PATH_DASHBOARD.admin.institut.prices(institutId));
+    };
+
+    const handleFilterName = (filtername) => {
+        setFilterName(filtername);
+        setPage(0);
+    };
+
+
     return (
         <>
             <Helmet>
-                <title> Administration: Sessions | Get-Tested</title>
+                <title> Administration: Instituts | Get-Tested</title>
             </Helmet>
             <Container maxWidth={themeStretch ? false : 'lg'}>
                 <CustomBreadcrumbs
-                    heading="Administration des sessions"
+                    heading="Administration des instituts"
                     links={[
                         { name: 'Dashboard', href: PATH_DASHBOARD.root },
-                        { name: 'Sessions' }
+                        { name: 'Instituts' }
                     ]}
+                    action={
+                        <Button
+                            variant="contained"
+                            startIcon={<Iconify icon="eva:plus-fill" />}
+                            component={RouterLink}
+                            to={PATH_DASHBOARD.admin.institut.create}
+                        >
+                            Nouvel Institut
+                        </Button>
+                    }
                 />
                 <Card>
-                    AdminSessionsPage Component
+                    <InstitutTableToolbar
+                        filterName={filterName}
+                        onFilterName={handleFilterName}
+                    />
+
+                    <TableContainer sx={{ position: 'relative', overflow: 'unset' }}>
+                        <TableSelectedAction
+                            dense={dense}
+                            numSelected={selected.length}
+                            rowCount={tableData.length}
+                            onSelectAllRows={(checked) =>
+                                onSelectAllRows(
+                                    checked,
+                                    tableData.map((row) => row.institut_id)
+                                )
+                            }
+                            action={
+                                <Tooltip title="Suppression des instituts sélectionnés">
+                                    <span>
+                                        <IconButton disabled color="primary" onClick={handleOpenConfirm}>
+                                            <Iconify icon='eva:trash-2-outline' />
+                                        </IconButton>
+                                    </span>
+                                </Tooltip>
+                            }
+                        />
+                        <Scrollbar>
+                            <Table size={dense ? 'small' : 'medium'}>
+                                <TableHeadCustom
+                                    order={order}
+                                    orderBy={orderBy}
+                                    headLabel={TABLE_HEAD}
+                                    rowCount={tableData.length}
+                                    numSelected={selected.length}
+                                    onSort={onSort}
+                                    onSelectAllRows={(checked) =>
+                                        onSelectAllRows(
+                                            checked,
+                                            tableData.map((row) => row.institut_id)
+                                        )
+                                    }
+                                />
+                                <TableBody>
+                                    {(isLoading ? [...Array(rowsPerPage)] : dataFiltered)
+                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                        .map((row, index) =>
+                                            row ? (
+                                                <InstitutTableRow
+                                                    key={row.institut_id}
+                                                    row={row}
+                                                    selected={selected.includes(row.institut_id)}
+                                                    onSelectRow={() => onSelectRow(row.institut_id)}
+                                                    onDeleteRow={() => handleDeleteRow(row.institut_id)}
+                                                    onEditRow={() => handleEditRow(row.institut_id)}
+                                                    onDetailRow={() => handleDetailRow(row.institut_id)}
+                                                    onUsersListRow={() => handleUsersList(row.institut_id)}
+                                                    onExaminatorsListRow={() => handleExaminatorsList(row.institut_id) }
+                                                    onSessionsListRow={() => handleSessionsList(row.institut_id)}
+                                                    onPricesListRow={() => handlePricesList(row.institut_id)}
+                                                />
+                                            ) : (
+                                                !isNotFound && <TableSkeleton key={index} sx={{ height: denseHeight }} />
+                                            )
+                                        )}
+
+                                    <TableEmptyRows height={denseHeight} emptyRows={emptyRows(page, rowsPerPage, tableData.length)} />
+
+                                    <TableNoData isNotFound={isNotFound} />
+                                </TableBody>
+                            </Table>
+                        </Scrollbar>
+                    </TableContainer>
+                    <TablePaginationCustom
+                        count={dataFiltered.length}
+                        page={page}
+                        rowsPerPage={rowsPerPage}
+                        onPageChange={onChangePage}
+                        onRowsPerPageChange={onChangeRowsPerPage}
+                        //
+                        dense={dense}
+                        onChangeDense={onChangeDense}
+                    />
                 </Card>
             </Container>
+
+            <ConfirmDialog
+                open={openConfirm}
+                onClose={handleCloseConfirm}
+                title="Delete"
+                content={
+                    <>
+                        Voulez-vous vraiment supprimer <strong> {selected.length} </strong> instituts ?
+                    </>
+                }
+                action={
+                    <Button
+                        variant="contained"
+                        color="error"
+                        onClick={() => {
+                            handleDeleteRows(selected);
+                            handleCloseConfirm();
+                        }}
+                    >
+                        Delete
+                    </Button>
+                }
+            />
         </>
     );
+}
+// ----------------------------------------------------------------------
+
+function applyFilter({ inputData, comparator, filterName }) {
+    const stabilizedThis = inputData.map((el, index) => [el, index]);
+
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+
+    inputData = stabilizedThis.map((el) => el[0]);
+
+    if (filterName) {
+        inputData = inputData.filter((item) => item.label.toLowerCase().indexOf(filterName.toLowerCase()) !== -1);
+    }
+
+    return inputData;
 }
---
+++ src/sections/@dashboard/session/list/SessionTableRow.js
@@ -0,0 +1,191 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { paramCase } from 'change-case';
+// @mui
+import {
+    TableRow,
+    Checkbox,
+    TableCell,
+    Typography,
+    MenuItem,
+    Alert,
+    IconButton,
+    Button
+} from '@mui/material';
+
+// components
+import Iconify from "../../../../components/iconify";
+import MenuPopover from '../../../../components/menu-popover';
+import ConfirmDialog from '../../../../components/confirm-dialog';
+//
+
+// ----------------------------------------------------------------------
+
+SessionTableRow.propTypes = {
+    row: PropTypes.object,
+    selected: PropTypes.bool,
+    onEditRow: PropTypes.func,
+    onSelectRow: PropTypes.func,
+    onDeleteRow: PropTypes.func,
+    onDetailRow: PropTypes.func,
+    onUsersListRow: PropTypes.func,
+    onExaminatorsListRow: PropTypes.func,
+    onSessionsListRow: PropTypes.func,
+    onPricesListRow: PropTypes.func
+
+};
+export default function SessionTableRow({
+                                             row,
+                                             selected,
+                                             onEditRow,
+                                             onSelectRow,
+                                             onDeleteRow,
+                                             onDetailRow,
+                                             onUsersListRow,
+                                             onExaminatorsListRow,
+                                             onSessionsListRow,
+                                             onPricesListRow
+                                         }) {
+    // eslint-disable-next-line camelcase
+    const { label, city, phone, email, institutCountry: { label : country } } = row;
+
+    const [openConfirm, setOpenConfirm] = useState(false);
+
+    const [openPopover, setOpenPopover] = useState(null);
+
+    const handleOpenConfirm = () => {
+        setOpenConfirm(true);
+    };
+    const handleCloseConfirm = () => {
+        setOpenConfirm(false);
+    };
+    const handleOpenPopover = (event) => {
+        setOpenPopover(event.currentTarget);
+    };
+
+    const handleClosePopover = () => {
+        setOpenPopover(null);
+    };
+
+    return (
+        <>
+            <TableRow hover selected={selected}>
+                <TableCell padding="checkbox">
+                    <Checkbox checked={selected} onClick={onSelectRow} />
+                </TableCell>
+                <TableCell sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography variant="subtitle2" noWrap>{label}</Typography>
+                </TableCell>
+                <TableCell> <Iconify icon={`twemoji:flag-${paramCase(country)}`} /> {country} </TableCell>
+                <TableCell> {city} </TableCell>
+                <TableCell> {email} </TableCell>
+                <TableCell> {phone} </TableCell>
+                <TableCell align="right">
+                    <IconButton color={openPopover ? 'primary' : 'default'} onClick={handleOpenPopover}>
+                        <Iconify icon="eva:more-vertical-fill" />
+                    </IconButton>
+                </TableCell>
+            </TableRow>
+
+            <MenuPopover
+                open={openPopover}
+                onClose={handleClosePopover}
+                arrow="right-top"
+                sx={{ width: 140 }}
+            >
+                <MenuItem
+                    onClick={() => {
+                        onDetailRow();
+                        handleClosePopover();
+                    }}
+                >
+                    <Iconify icon='icon-park-outline:doc-detail' />
+                    Détail
+                </MenuItem>
+
+
+                <MenuItem
+                    onClick={() => {
+                        onUsersListRow();
+                        handleClosePopover();
+                    }}
+                >
+                    <Iconify icon='clarity:users-solid' />
+                    Inscrits
+                </MenuItem>
+
+
+                <MenuItem
+                    onClick={() => {
+                        onExaminatorsListRow();
+                        handleClosePopover();
+                    }}
+                >
+                    <Iconify icon='fa-solid:chalkboard-teacher' />
+                    Examinateurs
+                </MenuItem>
+
+
+                <MenuItem
+                    onClick={() => {
+                        onSessionsListRow();
+                        handleClosePopover();
+                    }}
+                >
+                    <Iconify icon='entypo:price-ribbon' />
+                    Sessions
+                </MenuItem>
+
+                <MenuItem
+                    onClick={() => {
+                        onPricesListRow();
+                        handleClosePopover();
+                    }}
+                >
+                    <Iconify icon='ic:baseline-euro' />
+                    Tarifs
+                </MenuItem>
+
+
+                <MenuItem
+                    onClick={() => {
+                        onEditRow();
+                        handleClosePopover();
+                    }}
+                >
+                    <Iconify icon='clarity:edit-solid' />
+                    Edit
+                </MenuItem>
+
+
+                <MenuItem
+                    onClick={() => {
+                        handleOpenConfirm();
+                        handleClosePopover();
+                    }}
+                    sx={{ color: 'error.main' }}
+                >
+                    <Iconify icon='ep:delete-filled' />
+                    Delete
+                </MenuItem>
+            </MenuPopover>
+
+            <ConfirmDialog
+                open={openConfirm}
+                onClose={handleCloseConfirm}
+                title="Suppression d'un institut"
+                content= "Supprimer définitivement l'institut et ses données? (Action irréversible)"
+                action={
+                    <Button variant="contained" color="error" onClick={onDeleteRow}>
+                        Delete
+                    </Button>
+
+                }
+            />
+
+
+
+        </>
+    );
+
+}
