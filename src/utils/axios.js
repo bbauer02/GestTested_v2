5facea4194a2768c547de46cb2a0c4d52e7d0f2d
bbauer02 <bbauer02@gmail.com>
2024-01-21 23:40:03 +0100

    changement de parametre de proxy API

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,10 @@ import { HOST_API_KEY } from '../config-global';
 
 // ----------------------------------------------------------------------
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+    baseURL: HOST_API_KEY,
+    withCredentials: true
+});
 
 axiosInstance.interceptors.response.use(
   (response) => response,
